c7d9810a66976a90f53f84fdb068a6d8483f9c6d
Deepak <deepak.hsn29@gmail.com>
2025-11-24 23:14:24 +0530

    Add deployment test app

---
+++ app/hello_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello Test", page_icon="👋")
+
+st.title("Hello from Streamlit Cloud")
+st.write("If you can see this, the platform is fine and the big app is the problem.")
